9a6cf11d2a5ffe2cd453e5bfe666045c76b31fa9
Michał Morawski <morawskimichal@endor.local>
2019-10-21 11:29:16 +0200

    zmienne

---
+++ Strona/zmienne.js
@@ -0,0 +1,43 @@
+var a = true;
+var b = 3;
+
+var zmiennaPierwsza;
+let ZmiennaPierwsza;
+let zmienna_pierwsza;
+
+var str = 'aaa',
+    str2 = 'bbb';
+
+var str = 'aaa';
+var str2 = 'bbb';
+
+if(true) {
+
+}
+
+
+
+
+var vat22 = 0.22;
+document.write('<b>' + (a * vat22) + '</b>');
+
+
+
+
+
+var func1 = function() {
+    return 1;
+}
+var bool = 'false';
+var I = 50;
+var i;
+var j;
+var len;
+var hei;
+
+if(bool == true) {
+
+}
+if(typeof bool == 'boolean') {
+
+}
