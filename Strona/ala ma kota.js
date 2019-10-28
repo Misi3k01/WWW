8b3380593cfbc57749fd1cbf36690af0cd92907e
Michał Morawski <morawskimichal@endor.local>
2019-10-28 11:17:06 +0100

    9

---
+++ Strona/ala ma kota.js
@@ -0,0 +1,26 @@
+var text = 'Ala ma kota';
+for(let i = 0; i < text.length; i++)
+    document.writeln(text[i]);
+
+var num = 16;
+for(let i = num; i > 0; i--)
+    console.log(i.toString(2))
+
+var num = 16;
+for(let i = num; i > 0; i--)
+    console.log(i.toString(16))
+
+var num = 16;
+for(let i = num; i > 0; i--)
+    console.log(i.toString(8))
+
+var hex = 'ce';
+alert(parseInt(hex, 16));
+
+var float = 109.67;
+console.log(parseInt(float));  //ucina czesc po przecinku
+
+var f_text = '399.5677';
+console.log(parseFloat(f_text));
+console.log(parseFloat(f_text) + float);
+console.log(f_text + float);
