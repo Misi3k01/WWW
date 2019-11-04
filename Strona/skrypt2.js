ec01d85bc2943bbbb9d0899b1766efc3b916813e
Michał Morawski <morawskimichal@endor.local>
2019-11-04 11:24:20 +0100

    55

---
+++ Strona/skrypt2.js
@@ -0,0 +1,56 @@
+let x = 2;
+let y = 5;
+let z = 0;
+
+console.log(z = x + y);
+console.log(z = x - y);
+console.log(z = x * y);
+console.log(z = x / y);
+console.log(z = x % y); //reszta z dzielenia (dzielenie modulowe)
+console.log(z = x ** y); //potęgowanie
+
+console.log(z = ++x);
+console.log(z = x++);
+
+console.log(z = --x);
+console.log(z = x--);
+
+z = 7;
+z += x; // z = z + x;
+z -= x; // z = z - x;
+z *= x; // z = z * x;
+z /= x; // z = z / x;
+z %= x; // z = z % x;
+
+x += 5;
+
+let txt = "Ala";
+    txt += "ma";
+    txt += "kota";
+
+if(x == 2) {
+    console.log("x = 2");
+}
+if (y != 6) {
+    console.log("y != 6");
+}
+if (x === "2") {
+    console.log("x === 2");
+} else {
+    console.log("x !=== 2");
+}
+if (x === 2) {
+    console.log("x === 2");
+}
+
+if(x > 7) {
+    console.log("x > 7");
+} else {
+    console.log("x <= 7")
+}
+
+if(x < 7) {
+    console.log("x < 7");
+} else {
+    console.log("x >= 7")
+}
